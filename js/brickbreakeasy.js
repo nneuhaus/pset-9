d137ef135ff0a82a9caee4b6b732f0e481db9377
Nathalie Neuhaus <nathdancer@gmail.com>
2020-03-01 17:12:38 -0500

    decided to add levels to bb

--- js/brickbreakeasy.js
+++ js/brickbreakeasy.js
@@ -1,145 +1,44 @@
 ///////////////////// CONSTANTS /////////////////////////////////////
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
-var canvas = document.getElementById("myCanvas");
-
-var ctx = canvas.getContext("2d");
-
-var canvas = document.getElementById("myCanvas");
-
-var ctx = canvas.getContext("2d");
-
-var x = canvas.width/2;
-
-var y = canvas.height-30;
-
-var dx = 2;
-
-var dy = -2;
-
-
-
-var ballRadius = 10;
-
-
-
-var paddleHeight = 10;
-
-var paddleWidth = 75;
-
-var paddleX = (canvas.width-paddleWidth) / 2;
-
-
-
-var rightPressed = false;
-
-var leftPressed = false;
-
-
-
-var brickRowCount = 3;
-
-var brickColumnCount = 5;
-
-var brickWidth = 75;
-
-var brickHeight = 20;
-
-var brickPadding = 10;
-
-var brickOffsetTop = 30;
-
-var brickOffsetLeft = 30;
-
-var score = 0;
-
-var lives = 3;
 
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
-///////////////////// EVENT LISTENERS ///////////////////////////////
-///////////////////// FUNCTIONS /////////////////////////////////////
-
 var canvas = document.getElementById("myCanvas");
-
 var ctx = canvas.getContext("2d");
-
 var canvas = document.getElementById("myCanvas");
-
 var ctx = canvas.getContext("2d");
-
 var x = canvas.width/2;
-
 var y = canvas.height-30;
-
 var dx = 2;
-
 var dy = -2;
-
-
-
 var ballRadius = 10;
-
-
-
 var paddleHeight = 10;
-
 var paddleWidth = 75;
-
 var paddleX = (canvas.width-paddleWidth) / 2;
-
-
-
 var rightPressed = false;
-
 var leftPressed = false;
-
-
-
 var brickRowCount = 3;
-
-var brickColumnCount = 5;
-
+var brickColumnCount = 6;
 var brickWidth = 75;
-
 var brickHeight = 20;
-
 var brickPadding = 10;
-
 var brickOffsetTop = 30;
-
 var brickOffsetLeft = 30;
-
 var score = 0;
-
 var lives = 3;
-
-
-
 var bricks = [];
-
 for(var c=0; c<brickColumnCount; c++) {
-
     bricks[c] = [];
-
     for(var r=0; r<brickRowCount; r++) {
-
         bricks[c][r] = { x: 0, y: 0, status: 1 };
 
     }
 
 }
-
-
-
+///////////////////// EVENT LISTENERS ///////////////////////////////
 document.addEventListener("keydown", keyDownHandler, false);
-
 document.addEventListener("keyup", keyUpHandler, false);
-
 document.addEventListener("mousemove", mouseMoveHandler, false);
-
-
-
-
-
+///////////////////// FUNCTIONS /////////////////////////////////////
 function mouseMoveHandler(e) {
 
     var relativeX = e.clientX - canvas.offsetLeft;
